22832ac5e982a0de36d0d103a8f89d4d836a89ce
Syerne <shubhamyerne1994@gmail.com>
2022-05-28 12:20:49 +0530

    Added: Doublet coin flip program

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,26 +1,40 @@
 echo "welcome to the flip coin combination"
-
 head=1
-declare -A Singlet
+declare -A Doublet
 
 for ((i=1; i<=50; i++))
 do
-        randomCheck=$((RANDOM % 2))
-        if [ $randomCheck == 1 ]
+	randomCheck1=$((RANDOM % 2))
+	randomCheck2=$((RANDOM % 2))
+
+	if (( $randomCheck1 == 1 && $randomCheck2 == 1 ))
+	then
+        	echo "H H"
+		((Doublet[HH]++))
+	elif (( $randomCheck1 == 0 && $randomCheck2 == 1 ))
+	then
+		echo "T H"
+		((Doublet[HT]++))
+	elif (( $randomCheck1 == 1 && $randomCheck2 == 0 ))
         then
-                echo "HEADS"
-                ((Singlet[H]++))
-        else
-                echo "TAILS"
-                ((Singlet[T]++))
-        fi
+                echo "H T"
+                ((Doublet[TH]++))
+	
+	else
+		echo "T T"
+		((Doublet[TT]++))
+	fi
 done
 
-echo ${!Singlet[@]}
-echo ${Singlet[@]}
+echo ${!Doublet[@]}
+echo ${Doublet[@]}
 
-HeadPercentage=`awk "BEGIN {print (${Singlet[H]}/50)*100}"`
-echo "HEAD = $HeadPercentage%"
-TailPercentage=`awk "BEGIN {print (${Singlet[T]}/50)*100}"`
-echo "TAIL = $TailPercentage%"
+HH=`awk "BEGIN {print (${Doublet[HH]}/50)*100}"`
+echo "HH = $HH%"
+HT=`awk "BEGIN {print (${Doublet[HT]}/50)*100}"`
+echo "HT = $HT%"
+TH=`awk "BEGIN {print (${Doublet[TH]}/50)*100}"`
+echo "TH = $TH%"
+TT=`awk "BEGIN {print (${Doublet[TT]}/50)*100}"`
+echo "TT = $TT%"
 
