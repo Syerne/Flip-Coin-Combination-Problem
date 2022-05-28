da13c81d2d69461f1653ef73ac5bb559f1dad050
Syerne <shubhamyerne1994@gmail.com>
2022-05-28 10:14:31 +0530

    Modify: singlet program

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,26 @@
 echo "welcome to the flip coin combination"
 
-Head=1
-randomcheck=$((RANDOM%2 ))
-if(( randomcheck==1 ))
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+head=1
+declare -A Singlet
+
+for ((i=1; i<=50; i++))
+do
+        randomCheck=$((RANDOM % 2))
+        if [ $randomCheck == 1 ]
+        then
+                echo "HEADS"
+                ((Singlet[H]++))
+        else
+                echo "TAILS"
+                ((Singlet[T]++))
+        fi
+done
+
+echo ${!Singlet[@]}
+echo ${Singlet[@]}
+
+HeadPercentage=`awk "BEGIN {print (${Singlet[H]}/50)*100}"`
+echo "HEAD = $HeadPercentage%"
+TailPercentage=`awk "BEGIN {print (${Singlet[T]}/50)*100}"`
+echo "TAIL = $TailPercentage%"
+
